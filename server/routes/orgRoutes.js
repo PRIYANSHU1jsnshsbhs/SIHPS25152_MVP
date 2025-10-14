49d9998e50a661419381ab1084c42c551d0d6167
codewithabhay10 <abhaymadan22@gmail.com>
2025-10-14 22:36:32 +0530

    server removed

--- server/routes/orgRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { protect, authorize } from "../middleware/authMiddleware.js";
-import { getApplications, verifyApplication } from "../controllers/orgController.js";
-
-const router = express.Router();
-router.get("/applications", protect, authorize("organization"), getApplications);
-router.post("/verify/:id", protect, authorize("organization"), verifyApplication);
-export default router;
